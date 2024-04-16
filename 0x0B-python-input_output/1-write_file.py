1df8823400f3f398cd62f5b2552b95744d661081
zougmar <zougmar123@gmail.com>
2024-04-16 05:45:42 -0700

    c

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a file-writing function."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file."""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
