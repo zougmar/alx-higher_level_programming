7b7fa130191400bcd55ea4cb82ececa140a8aba2
zougmar <zougmar123@gmail.com>
2024-04-16 05:42:05 -0700

    c

--- 0x0B-python-input_output/0-read_file.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module containing the function read_file"""
-
-
-def read_file(filename=""):
-    """Reads a file and prints to stdout"""
-    with open(filename, 'r', encoding="utf-8") as f:
-        read_data = f.read()
-        print(read_data, end='')
